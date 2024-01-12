e204cc8c68e8d54b64198c55fa888af129bdc644
Faduma <aladirie@gmail.com>
2024-01-12 18:38:37 -0500

    created phtoto list item and add imports in App file

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,20 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+    const PhotoListItem = ({ photo }) => {
+    return (
+      <div className="photo-list-item">
+      <img src={photo.imageSource} alt={`Photo ${photo.id}`} />
+        <div className="photographer-info">
+        <img src={photo.profile} alt={`${photo.username}'s profile`} />
+        <span className="username">{photo.username}</span>
+      </div>
+        <div className="photo-location">
+        {photo.location.city}, {photo.location.country}
+        </div>
+      </div>
+    );
 };
 
 export default PhotoListItem;
