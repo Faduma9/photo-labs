b74054df78856a06447a9f2fd2d85f4deafe0d88
Faduma <aladirie@gmail.com>
2024-01-12 21:14:28 -0500

    applied Styling to the Photo Component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,21 +2,19 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-
-    const PhotoListItem = ({ photo }) => {
-    return (
-      <div className="photo-list-item">
-      <img src={photo.imageSource} alt={`Photo ${photo.id}`} />
-        <div className="photographer-info">
-        <img src={photo.profile} alt={`${photo.username}'s profile`} />
-        <span className="username">{photo.username}</span>
+const PhotoListItem = ({ photo }) => {
+  return (
+    <div className="photo-list__item">
+    <img src={photo.imageSource} alt={`Photo ${photo.id}`} className="photo-list__image" />
+    <div className="photo-list__user-details">
+      <img src={photo.profile} alt={`${photo.username}'s profile`} className="photo-list__user-profile" />
+      <span className="photo-list__user-info">{photo.username}</span>
+    </div>
+    <div className="photo-list__user-location">
+      {photo.location.city}, {photo.location.country}
       </div>
-        <div className="photo-location">
-        {photo.location.city}, {photo.location.country}
-        </div>
-      </div>
-    );
+    </div>
+  );
 };
 
 export default PhotoListItem;
