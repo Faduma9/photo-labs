552ee77c2d1e0cd1f585e7f6533f9b8c83a14928
Faduma <aladirie@gmail.com>
2024-01-18 17:21:16 -0500

    created hooks file and used application data hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import photos from './mocks/photos';
 import topics from './mocks/topics';
 import HomeRoute from './routes/HomeRoute';
 import PhotoDetailsModal from './routes/PhotoDetailsModal'; 
+import useApplicationData from './hooks/useApplicationData';
 
 const App = () => {
-  const [modalState, setModalState] = useState({ 
-    displayModal: false, 
-    selectedPhoto: null,
-    similarPhotos: [] // Add this new state
-  });
-
-  const setDisplayModal = (display) => {
-    setModalState({ ...modalState, displayModal: display });
-  };
-
-  const setSelectedPhoto = (photo) => {
-    const similarPhotos = photos.filter(p => 
-      p.location.city === photo.location.city && p.id !== photo.id
-    );
-
-    setModalState({ 
-      ...modalState, 
-      selectedPhoto: photo, 
-      similarPhotos: similarPhotos // Update the state with similar photos
-    });
-  };
-
+  const {
+    modalState,
+    setModalState,
+    favorites,
+    toggleFavorite,
+    setDisplayModal,
+    setSelectedPhoto
+  } = useApplicationData();
+  
   return (
     <div className="App">
       <HomeRoute
-        favoriteCount={5}
         photoData={photos}
         topicData={topics}
         setDisplayModal={setDisplayModal}
         setSelectedPhoto={setSelectedPhoto}
+        favorites={favorites}
+        toggleFavorite={toggleFavorite}
       />
       {modalState.displayModal && (
         <PhotoDetailsModal
           setDisplayModal={setDisplayModal}
           selectedPhoto={modalState.selectedPhoto}
           similarPhotos={modalState.similarPhotos} // Pass similar photos to the modal
+          setSelectedPhoto={setSelectedPhoto}
+          favorites={favorites}
+          toggleFavorite={toggleFavorite}
         />
       )}
     </div>
   );
 };
-
-
 export default App;
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,45 +1,61 @@
-import React from 'react';
-
-import '../styles/PhotoDetailsModal.scss'
+import React, { useEffect } from 'react';
+import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoListItem from '../components/PhotoListItem';
+import PhotoFavButton from '../components/PhotoFavButton';
 
+const PhotoDetailsModal = ({ setDisplayModal, setSelectedPhoto, selectedPhoto, similarPhotos, toggleFavorite, favorites }) => {
+  useEffect(() => {
+    console.log(selectedPhoto);
+  }, [selectedPhoto]); // Add similarPhotos as a dependency
 
-const PhotoDetailsModal = ({ setDisplayModal, selectedPhoto, similarPhotos }) => {
   const handleClose = () => {
     setDisplayModal(false);
   };
-
+  const handleSetSelectedPhoto = (photo) => {
+    setSelectedPhoto(photo);
+  };
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={handleClose}>
         <img src={closeSymbol} alt="Close" />
       </button>
-
       {/* Displaying the larger selected photo */}
       {selectedPhoto && (
         <div className="photo-details-modal__images">
+          <PhotoFavButton isLiked={favorites.has(selectedPhoto.id)} onToggle={() => toggleFavorite(selectedPhoto.id)} />
           <img src={selectedPhoto.urls.full} alt={`Photo ${selectedPhoto.id}`} className="photo-details-modal__image" />
+          <div className="photo-details-modal__photographer">
+      <img src={selectedPhoto.profile} alt={`Photographer ${selectedPhoto.username}`} className="photo-details-modal__photographer-profile" />
+      {selectedPhoto.username}
+      </div>
         </div>
       )}
 
       {/* Similar Photos Section */}
-      <h2 className="photo-details-modal__header">Similar Photos</h2>
+      <h2 className="photo-details-modal__header">Similar Photo</h2>
       <div className="photo-details-modal__images">
-        {similarPhotos && similarPhotos.map(photo => (
-          <PhotoListItem
-            key={photo.id}
-            id={photo.id}
-            location={photo.location}
-            imageSource={photo.urls.regular}
-            username={photo.user.username}
-            profile={photo.user.profile}
-          />
-        ))}
+        {similarPhotos && similarPhotos.length > 0 ? (
+          similarPhotos.map(photo => (
+            <PhotoListItem
+              key={photo.id}
+              id={photo.id}
+              location={photo.location}
+              urls={photo.urls}
+              username={photo.user.username}
+              profile={photo.user.profile}
+              isFavorited={favorites.has(photo.id)}
+              onToggleFavorite={() => toggleFavorite(photo.id)}
+              setDisplayModal={setDisplayModal}
+              setSelectedPhoto={handleSetSelectedPhoto}
+            />
+          ))
+        ) : (
+          <p></p>
+        )}
       </div>
     </div>
   );
 };
-
-
+ 
 export default PhotoDetailsModal;
